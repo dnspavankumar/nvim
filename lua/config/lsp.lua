5fa96f6fa5c0c5fe10e8672836f7ee48715ce920
lnb <lnbsa12345@gmail.com>
2026-03-15 21:19:30 +0530

    add a whole lot of lsp keymaps

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,3 +1,110 @@
+local function on_attach(client, bufnr)
+  local map = function(lhs, rhs, desc)
+    vim.keymap.set("n", lhs, rhs, { buffer = bufnr, silent = true, desc = desc })
+  end
+  local del = vim.keymap.del
+  del('n', '[d')
+  del('n', ']d')
+
+  map('K', function() vim.lsp.buf.hover() end, 'Hover')
+  map('gd', function() require 'fzf-lua'.lsp_definitions() end, 'Goto Definition')
+  map('gD', function() require 'fzf-lua'.lsp_declarations() end, 'Goto Declaration')
+  map("gi", function() require 'fzf-lua'.lsp_implementations() end, "Go to implementation")
+  map('gr', function() require 'fzf-lua'.lsp_references() end, "List references")
+
+  map('<leader>ca', function() vim.lsp.buf.code_action() end, 'Code Action')
+  map('<leader>cr', function() vim.lsp.buf.rename() end, 'Rename')
+  map('<leader>cf', function() vim.lsp.buf.format() end, 'Lsp Format')
+  map('<leader>cd', function() vim.diagnostic.open_float() end, 'Line Diagnostics')
+  map('<leader>sd', function() require 'fzf-lua'.diagnostics_document() end, 'Document Diagnostics')
+  map('<leader>sD', function() require 'fzf-lua'.diagnostics_workspace() end, 'Workspace Diagnostics')
+
+  map('[d', function() vim.diagnostic.jump { count = -1, float = false } end, 'Previous Diagnostic')
+  map(']d', function() vim.diagnostic.jump { count = 1, float = false } end, 'Next Diagnostic')
+  map('[e', function() vim.diagnostic.jump { count = -1, float = false, severity = 'ERROR' } end,
+    'Previous Error')
+  map(']e', function() vim.diagnostic.jump { count = 1, float = false, severity = 'ERROR' } end,
+    'Next Error')
+
+  local filter = require 'utils.plugins'.symbols_filter
+  map('<leader>ss', function()
+    require 'fzf-lua'.lsp_document_symbols { regex_filter = filter }
+  end, 'Goto Symbol')
+  map('<leader>sS', function()
+    require 'fzf-lua'.lsp_live_workspace_symbols { regex_filter = filter }
+  end, 'Goto Symbol')
+
+  vim.keymap.set({ 'n', 'i' }, '<c-h>', function()
+    local r, c = unpack(vim.api.nvim_win_get_cursor(0))
+    local l = vim.api.nvim_get_current_line()
+    if l:sub(c + 1, c + 1) == '(' then
+      vim.api.nvim_win_set_cursor(0, { r, c + 1 })
+    end
+    vim.defer_fn(vim.lsp.buf.signature_help, 1)
+  end, { desc = 'Signature Help', buffer = bufnr })
+
+  -- format on save
+  if client:supports_method 'textDocument/formatting' then
+    local grp = vim.api.nvim_create_augroup('LspFormat' .. bufnr, { clear = true })
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      group = grp,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format { bufnr = bufnr }
+      end,
+    })
+  end
+
+  -- breadcrumbs
+  if client:supports_method 'textDocument/documentSymbol' then
+    local ok_navic, navic = pcall(require, 'nvim-navic')
+    if ok_navic then
+      navic.attach(client, bufnr)
+    end
+  end
+
+  -- inlayhints
+  if client:supports_method 'textDocument/inlayHint' then
+    vim.defer_fn(function()
+      vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+    end, 0)
+  end
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp", "objc", "objcpp" },
+  callback = function(args)
+    if vim.fn.executable("clangd") ~= 1 then
+      return
+    end
+
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    local ok, blink = pcall(require, "blink.cmp")
+    if ok then
+      capabilities = blink.get_lsp_capabilities(capabilities)
+    end
+
+    local clangd_config = {
+      name = "clangd",
+      cmd = {
+        'clangd',
+        '--background-index',
+        '--clang-tidy',
+        '--header-insertion=iwyu',
+        '--completion-style=detailed',
+        '--function-arg-placeholders',
+        '--fallback-style=llvm',
+      },
+      capabilities = capabilities,
+      root_dir = vim.fs.root(args.buf, { "compile_commands.json", "compile_flags.txt", ".git" }),
+      on_attach = on_attach,
+    }
+
+    vim.lsp.config("clangd", clangd_config)
+    vim.lsp.enable("clangd")
+  end,
+})
+
 -- diagnostics config
 vim.api.nvim_create_autocmd('User', {
   pattern = 'VeryLazy',
@@ -24,60 +131,4 @@ vim.api.nvim_create_autocmd('User', {
       },
     }
   end
-})
-
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "c", "cpp", "objc", "objcpp" },
-  callback = function(args)
-    if vim.fn.executable("clangd") ~= 1 then
-      return
-    end
-
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    local ok, blink = pcall(require, "blink.cmp")
-    if ok then
-      capabilities = blink.get_lsp_capabilities(capabilities)
-    end
-
-    local on_attach = function(client, bufnr)
-      local map = function(lhs, rhs, desc)
-        vim.keymap.set("n", lhs, rhs, { buffer = bufnr, silent = true, desc = desc })
-      end
-
-      map("gd", vim.lsp.buf.definition, "Go to definition")
-      map("gD", vim.lsp.buf.declaration, "Go to declaration")
-      map("gi", vim.lsp.buf.implementation, "Go to implementation")
-      map("gr", vim.lsp.buf.references, "References")
-      map("K", vim.lsp.buf.hover, "Hover docs")
-      map("<leader>cf", function()
-        vim.lsp.buf.format({ async = true })
-      end, "Format file")
-
-      local ok_navic, navic = pcall(require, "nvim-navic")
-      if ok_navic and client.server_capabilities and client.server_capabilities.documentSymbolProvider then
-        navic.attach(client, bufnr)
-      end
-    end
-
-    local clangd_config = {
-      name = "clangd",
-      cmd = {
-        "clangd",
-        "--background-index",
-        "--completion-style=detailed",
-        "--header-insertion=never",
-      },
-      capabilities = capabilities,
-      root_dir = vim.fs.root(args.buf, { "compile_commands.json", "compile_flags.txt", ".git" }),
-      on_attach = on_attach,
-    }
-
-    if type(vim.lsp.config) == "function" and type(vim.lsp.enable) == "function" then
-      vim.lsp.config("clangd", clangd_config)
-      vim.lsp.enable("clangd")
-    else
-      vim.lsp.start(clangd_config)
-    end
-  end,
 })
